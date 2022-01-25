f9501ce6a858c9490f07adab5c9b85c7b5c37d65
uzayaltiner <92029942+uzayaltiner@users.noreply.github.com>
2022-01-26 01:15:41 +0300

    Initial Commit

---
+++ Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Uzay Altıner on 26.01.2022.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Uzay Altıner on 26.01.2022.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
